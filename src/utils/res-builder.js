b297372a22c1f73ee6be85ba64ed08ad70bc8a74
DenyStark <denyskrolevetskyi@gmail.com>
2019-07-21 15:50:11 +0300

    Fix: Errors messages

--- src/utils/res-builder.js
+++ src/utils/res-builder.js
@@ -1,7 +1,7 @@
 const errors = new Map();
 
-errors.set(73400, 'Wrong address or password.');
-errors.set(73401, 'No token provided.');
+errors.set(73400, 'No token provided.');
+errors.set(73401, 'Wrong token.');
 errors.set(73500, 'Internal server error.');
 
 const success = (data) => Object.assign({ status: 'success' }, data);
