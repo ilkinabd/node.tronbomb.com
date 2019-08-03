25c43da792be2870b76c15e09c62ef463748610c
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-03 16:36:16 +0300

    Ref: contract controller

--- src/controllers/contracts.js
+++ src/controllers/contracts.js
@@ -1,9 +1,11 @@
 const db = require('@db');
 
-const { resSuccess } = require('@utils/res-builder');
+const { resSuccess, resError } = require('@utils/res-builder');
 
 const getAll = async(_req, res) => {
   const contracts = await db.contracts.getAll();
+  if (contracts.length === 0) return res.status(500).json(resError(73500));
+
   res.json(resSuccess({ contracts }));
 };
 
