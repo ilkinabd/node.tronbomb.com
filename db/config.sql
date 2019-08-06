09cd83e4c328ec92fdef3ef647aa5f8b2ab83e1c
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-06 11:52:30 +0300

    Feat: Add Withdraw contract to db

--- db/config.sql
+++ db/config.sql
@@ -1,4 +1,4 @@
-CREATE TYPE CONTRACT_TYPE AS ENUM ('portal', 'dice', 'wheel', 'token');
+CREATE TYPE CONTRACT_TYPE AS ENUM ('portal', 'dice', 'wheel', 'token', 'withdraw');
 
 CREATE TABLE "contracts" (
   "id"      SERIAL        NOT NULL,
@@ -16,7 +16,8 @@ VALUES
   (DEFAULT, 'portal', 'TTF2udpEwuFtjd8YZjZm4mGCKfwc6KJsXY', NULL, 'Portal'),
   (DEFAULT, 'dice', 'TUoYPCJSPKuzvDmhGzSWPTokDKUfJbLtWi', NULL, 'Dice'),
   (DEFAULT, 'wheel', 'TE4Cerczq6YqNRpnLbamRy8Rw6eNZdp1Tr', NULL, 'Wheel'),
-  (DEFAULT, 'token', 'TAchv6odQC1PFPApdBJ1tNQ4uKTv2Yv4es', NULL, 'BOMB');
+  (DEFAULT, 'token', 'TAchv6odQC1PFPApdBJ1tNQ4uKTv2Yv4es', NULL, 'BOMB'),
+  (DEFAULT, 'withdraw', 'TFG1gZHC92QTYu1mLCGQA75xdQiq75n16Z', NULL, 'Withdraw');
 
 CREATE TABLE "sockets" (
   "socket_id" CHAR(20)      NOT NULL,
