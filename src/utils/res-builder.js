9b20677f91fd88bb4334759917d28fe0ed7cd229
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-17 23:28:45 +0300

    Feat: Update res-builder

--- src/utils/res-builder.js
+++ src/utils/res-builder.js
@@ -8,7 +8,18 @@ errors.set(73500, 'Internal server error.');
 const success = (data) => Object.assign({ status: 'success' }, data);
 const error = (code) => ({ status: 'error', code, message: errors.get(code) });
 
+const successRes = (res, data) => res.json(Object.assign({
+  status: 'success',
+}, data));
+const errorRes = (res, status, code) => res.status(status).json({
+  status: 'error',
+  code,
+  message: errors.get(code),
+});
+
 module.exports = {
+  successRes,
+  errorRes,
   resSuccess: success,
   resError: error,
 };
