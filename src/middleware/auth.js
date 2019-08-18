d87efd044bf141daf230d073b693fb720a5e2170
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-18 22:02:45 +0300

    Ref: auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,19 +1,19 @@
 const { TOKEN, ADMIN_TOKEN } = process.env;
 
-const { resError } = require('@utils/res-builder');
+const { errorRes } = require('@utils/res-builder');
 
 const server = (req, res, next) => {
   const token = req.headers['maxie-token'];
-  if (!token) return res.status(401).json(resError(73400));
-  if (token !== TOKEN) return res.status(403).json(resError(73401));
+  if (!token) errorRes(res, 401, 73400);
+  if (token !== TOKEN) errorRes(res, 403, 73401);
 
   next();
 };
 
 const admin = (req, res, next) => {
   const token = req.headers['maxie-token'];
-  if (!token) return res.status(401).json(resError(73400));
-  if (token !== ADMIN_TOKEN) return res.status(403).json(resError(73401));
+  if (!token) errorRes(res, 401, 73400);
+  if (token !== ADMIN_TOKEN) errorRes(res, 403, 73401);
 
   next();
 };
