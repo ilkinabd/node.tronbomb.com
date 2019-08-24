583a571f607dab74711f6c6e7a879d9de19deaab
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-24 17:36:32 +0300

    Hot fix: auth module

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,16 +4,16 @@ const { errorRes } = require('@utils/res-builder');
 
 const server = (req, res, next) => {
   const token = req.headers['maxie-token'];
-  if (!token) errorRes(res, 401, 73400);
-  if (token !== TOKEN) errorRes(res, 403, 73401);
+  if (!token) return errorRes(res, 401, 73400);
+  if (token !== TOKEN) return errorRes(res, 403, 73401);
 
   next();
 };
 
 const admin = (req, res, next) => {
   const token = req.headers['maxie-token'];
-  if (!token) errorRes(res, 401, 73400);
-  if (token !== ADMIN_TOKEN) errorRes(res, 403, 73401);
+  if (!token) return errorRes(res, 401, 73400);
+  if (token !== ADMIN_TOKEN) return errorRes(res, 403, 73401);
 
   next();
 };
