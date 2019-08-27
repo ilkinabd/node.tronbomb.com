6f61491938f853e89feaa11bf87cfc32fddcb13e
DenyStark <denyskrolevetskyi@gmail.com>
2019-08-27 23:29:19 +0300

    Ref: FreezeAll API

--- src/controllers/fund.js
+++ src/controllers/fund.js
@@ -74,19 +74,14 @@ const transferBOMB = async(req, res) => {
 const freezeAll = async(req, res) => {
   const { type } = req.body;
 
-  // Default period 365 days;
-  const period = 365;
-
   const { address, encryptedKey } = await db.funds.get({ type });
   if (!address) return errorRes(res, 422, 73407);
 
   const { amount } = await bombUtils.get.balanceOf(address);
-  if (amount < 10 ** 6) return successRes(res);
-
   const privateKey = decrypt(encryptedKey);
 
   console.info(`Freeze ${amount} BOMB from ${type} fund.`);
-  const result = await utils.freeze(amount, period, privateKey);
+  const result = await utils.freeze(amount, privateKey);
   if (result.error) return errorRes(res, 500, 73501, result.error);
 
   successRes(res);
--- src/utils/fund.js
+++ src/utils/fund.js
@@ -6,8 +6,8 @@ const address = db.contracts.get({ type: 'portal' });
 const transferBOMB = (to, amount, privateKey) =>
   send('transfer', address, privateKey)(to, amount);
 
-const freeze = (amount, period, privateKey) =>
-  send('freeze', address, privateKey)(amount, period);
+const freeze = (amount, privateKey) =>
+  send('freeze', address, privateKey)(amount);
 
 module.exports = {
   transferBOMB,
