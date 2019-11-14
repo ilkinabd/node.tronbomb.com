f2932c13f4137cef43b1990c51365927ecce1034
xterminate <ilkin5@mail.ru>
2019-11-14 12:25:15 +0300

    Adds game model, util, workers

---
+++ src/workers/coin.js
@@ -0,0 +1,27 @@
+const utils = require("@utils/coin");
+const models = require("@models/coin");
+
+const takePart = async (block, chanel) => {
+  const payload = await utils.events.takeBet(block);
+  if (!payload) return setTimeout(() => takePart(block, chanel), 1000);
+
+  for (const item of payload) {
+    const event = models.takeBet(item.result);
+    chanel.emit("coin-take-part", event);
+  }
+};
+
+const reward = async (block, chanel) => {
+  const payload = await utils.events.playersWin(block);
+  if (!payload) return setTimeout(() => reward(block, chanel), 1000);
+
+  for (const item of payload) {
+    const event = models.playerWin(item.result);
+    chanel.emit("coin-reward", event);
+  }
+};
+
+module.exports = async (block, chanel) => {
+  takePart(block, chanel);
+  reward(block, chanel);
+};
