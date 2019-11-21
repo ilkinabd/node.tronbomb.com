8e029889a5de8a2cf349a3708a8a69616b9f8b5f
Alexandr.Makarov <makar.is.code.jedi@gmail.com>
2019-11-21 23:00:33 +0200

    logs: add console log for referal profit

--- src/workers/operations.js
+++ src/workers/operations.js
@@ -15,6 +15,7 @@ const referralProfit = async(block, chanel) => {
   const payload = await utils.events.referralProfit(block);
   if (!payload) return setTimeout(() => referralProfit(block, chanel), 1000);
 
+  console.log('withdraw-referral-profit', payload);
   for (const item of payload) {
     const event = models.wallet(item.result);
     chanel.emit('withdraw-referral-profit', event);
