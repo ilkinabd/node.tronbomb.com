0c9a24c273d90de32e1264580edf4870befa8546
xterminate <ilkin5@mail.ru>
2020-01-08 02:21:24 +0300

    Comment game block handling

--- src/workers/blocks.js
+++ src/workers/blocks.js
@@ -12,14 +12,14 @@ module.exports = async io => {
     const current = await currentBlock() - 1;
     for (let block = lastBlock + 1; block <= current; block++) {
       console.log(`Proceed block ${block}`);
-      io.in('blocks').emit('blocks', block);
+      // io.in('blocks').emit('blocks', block);
 
-      dice(block, io.in('dice'));
-      coin(block, io.in("coin"));
-      wheel(block, io.in('wheel'));
-      operations(block, io.in('operations'));
-      bomb(block, io.in('bomb'));
-      auction(block, io.in('auction'));
+      // dice(block, io.in('dice'));
+      // coin(block, io.in("coin"));
+      // wheel(block, io.in('wheel'));
+      // operations(block, io.in('operations'));
+      // bomb(block, io.in('bomb'));
+      // auction(block, io.in('auction'));
     }
     lastBlock = current;
   }, 3000);
