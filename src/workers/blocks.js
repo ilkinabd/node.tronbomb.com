b1e6c7a4d32961fef9eb447074c89f1a1781385f
xterminate <ilkin5@mail.ru>
2020-01-23 17:33:53 +0300

    updates blocks handler

--- src/workers/blocks.js
+++ src/workers/blocks.js
@@ -1,11 +1,11 @@
 const { currentBlock } = require('@utils/tron');
 const wallet = require('@workers/wallet');
-// const dice = require("@workers/dice");
-// const coin = require("@workers/coin");
-// const wheel = require("@workers/wheel");
-// const operations = require("@workers/operations");
-// const bomb = require("@workers/bomb");
-// const auction = require("@workers/auction");
+const dice = require('@workers/dice');
+const coin = require('@workers/coin');
+const wheel = require('@workers/wheel');
+const operations = require('@workers/operations');
+const bomb = require('@workers/bomb');
+const auction = require('@workers/auction');
 
 module.exports = async io => {
   let lastBlock = (await currentBlock()).block_header;
@@ -14,14 +14,14 @@ module.exports = async io => {
     for (let block = lastBlock + 1; block <= current; block++) {
       console.log(`Proceed block ${block}`);
       wallet(block, io.in('wallet'));
-      // io.in('blocks').emit('blocks', block);
+      io.in('blocks').emit('blocks', block);
 
-      // dice(block, io.in('dice'));
-      // coin(block, io.in("coin"));
-      // wheel(block, io.in('wheel'));
-      // operations(block, io.in('operations'));
-      // bomb(block, io.in('bomb'));
-      // auction(block, io.in('auction'));
+      dice(block, io.in('dice'));
+      coin(block, io.in('coin'));
+      wheel(block, io.in('wheel'));
+      operations(block, io.in('operations'));
+      bomb(block, io.in('bomb'));
+      auction(block, io.in('auction'));
     }
     lastBlock = current;
   }, 3000);
